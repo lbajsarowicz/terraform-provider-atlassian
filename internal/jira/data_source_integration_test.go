98faad3557fc090cb06a48b1c1a1cb5ed88a64d6
Łukasz Bajsarowicz <lukasz@lbajsarowicz.me>
2026-04-03 18:56:58 +0200

    test: add integration tests for data sources

---
+++ internal/jira/data_source_integration_test.go
@@ -0,0 +1,70 @@
+package jira_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/lbajsarowicz/terraform-provider-atlassian/internal/testutil"
+)
+
+func TestIntegrationPermissionSchemeDataSource_basic(t *testing.T) {
+	testutil.SkipIfNoAcc(t)
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testutil.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+resource "atlassian_jira_permission_scheme" "test" {
+  name        = %q
+  description = "DS integration test (run %s)"
+}
+
+data "atlassian_jira_permission_scheme" "test" {
+  name = atlassian_jira_permission_scheme.test.name
+}
+`, rName, testutil.RunID()),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"data.atlassian_jira_permission_scheme.test", "id",
+						"atlassian_jira_permission_scheme.test", "id",
+					),
+					resource.TestCheckResourceAttr("data.atlassian_jira_permission_scheme.test", "name", rName),
+				),
+			},
+		},
+	})
+}
+
+func TestIntegrationScreenDataSource_basic(t *testing.T) {
+	testutil.SkipIfNoAcc(t)
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testutil.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+resource "atlassian_jira_screen" "test" {
+  name        = %q
+  description = "DS integration test (run %s)"
+}
+
+data "atlassian_jira_screen" "test" {
+  name = atlassian_jira_screen.test.name
+}
+`, rName, testutil.RunID()),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"data.atlassian_jira_screen.test", "id",
+						"atlassian_jira_screen.test", "id",
+					),
+					resource.TestCheckResourceAttr("data.atlassian_jira_screen.test", "name", rName),
+				),
+			},
+		},
+	})
+}
